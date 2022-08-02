affe0bdd5ac4b6fae18e5a4758c80bd4c0466bbc
fabianrios <fabian.rios@metals-hub.com>
2022-08-02 16:37:20 +0200

    first changes

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Fabián Ríos',
+    site_url: 'https://fabianrios.co',
+    feed_url: 'https://fabianrios.co/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
