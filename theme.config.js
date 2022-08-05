2712368336763bb755aae08395c13169d0ae41ad
fabianrios <fabian.rios@metals-hub.com>
2022-08-05 12:53:16 +0200

    main menu and scroll to top

--- theme.config.js
+++ theme.config.js
@@ -2,20 +2,23 @@ const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Fabián Ríos.
-      <a href="/feed.xml">RSS</a>
-      <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
+    <>
+      <small style={{ display: 'block', marginTop: '8rem' }}>
+        <time>{YEAR}</time> © Fabián Ríos.
+        <a href="/feed.xml">RSS</a>
+        <style jsx>{`
+          a {
+            float: right;
           }
-        }
-      `}</style>
-    </small>
+          @media screen and (max-width: 480px) {
+            article {
+              padding-top: 2rem;
+              padding-bottom: 4rem;
+            }
+          }
+        `}</style>
+      </small>
+      <a class="scroll-top" href="#">↑</a> 
+    </>
   )
 }
