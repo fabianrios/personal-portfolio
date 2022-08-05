b424959a73ed2840aa8fdb0a12a15f8a74cc0225
fabianrios <fabian.rios@metals-hub.com>
2022-08-05 12:19:06 +0200

    main variable typo for date function

--- components/yearDate.js
+++ components/yearDate.js
@@ -1,6 +1,6 @@
 export const YearDate = ({year}) => {
-  const [month, day, year] = year.split('-');
+  const [month, day, y] = year.split('-');
   const now = new Date().getFullYear();
-  const birth = new Date(+year, +month - 1, +day).getFullYear();
+  const birth = new Date(+y, +month - 1, +day).getFullYear();
   return now - birth || null;
 }
