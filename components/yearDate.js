53ceda844551eaede6bf1e329e4c9440b47191a2
fabianrios <fabian.rios@metals-hub.com>
2022-08-05 12:16:54 +0200

    main change date function to be supported on mobile

--- components/yearDate.js
+++ components/yearDate.js
@@ -1,5 +1,6 @@
 export const YearDate = ({year}) => {
+  const [month, day, year] = year.split('-');
   const now = new Date().getFullYear();
-  const birth = new Date(year).getFullYear();
-  return <>{now - birth || 9}</>;
+  const birth = new Date(+year, +month - 1, +day).getFullYear();
+  return now - birth || null;
 }
